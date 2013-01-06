9cab6da94aa9391affaf244a1287e2eb2ed37075
lix <lix_xu@jabil.com>
2013-01-06 16:04:09 +0800

    change version to 0.1.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='flask-paginate',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/lixxu/flask-paginate',
     license='BSD',
     author='Lix Xu',
