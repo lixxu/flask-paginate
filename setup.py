3d14d7fb7f7e2b1c40cf9e51284f3add7f28f775
James Turk <james.p.turk@gmail.com>
2015-05-21 18:00:53 -0400

    trove classifiers, fixes #32
    seem to be the intended versions

--- setup.py
+++ setup.py
@@ -29,6 +29,9 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
     ]
 )
